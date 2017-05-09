534eb2375c216df96fa90f544aeba99982c0cd35
EngMahmoudMagdy <engmahmoudmagdy11@gmail.com>
2017-05-09 11:31:01 +0200

    first

---
+++ Connect4.c
@@ -0,0 +1,256 @@
+// SpaceInvaders.c
+// Runs on LM4F120/TM4C123
+// Jonathan Valvano and Daniel Valvano
+// This is a starter project for the edX Lab 15
+// In order for other students to play your game
+// 1) You must leave the hardware configuration as defined
+// 2) You must not add/remove any files from the project
+// 3) You must add your code only this this C file
+// I.e., if you wish to use code from sprite.c or sound.c, move that code in this file
+// 4) It must compile with the 32k limit of the free Keil
+
+// April 10, 2014
+// http://www.spaceinvaders.de/
+// sounds at http://www.classicgaming.cc/classics/spaceinvaders/sounds.php
+// http://www.classicgaming.cc/classics/spaceinvaders/playguide.php
+/* This example accompanies the books
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+
+   "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+
+ Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+// ******* Required Hardware I/O connections*******************
+// Slide pot pin 1 connected to ground
+// Slide pot pin 2 connected to PE2/AIN1
+// Slide pot pin 3 connected to +3.3V 
+// fire button connected to PE0
+// special weapon fire button connected to PE1
+// 8*R resistor DAC bit 0 on PB0 (least significant bit)
+// 4*R resistor DAC bit 1 on PB1
+// 2*R resistor DAC bit 2 on PB2
+// 1*R resistor DAC bit 3 on PB3 (most significant bit)
+// LED on PB4
+// LED on PB5
+
+// Blue Nokia 5110
+// ---------------
+// Signal        (Nokia 5110) LaunchPad pin
+// Reset         (RST, pin 1) connected to PA7
+// SSI0Fss       (CE,  pin 2) connected to PA3
+// Data/Command  (DC,  pin 3) connected to PA6
+// SSI0Tx        (Din, pin 4) connected to PA5
+// SSI0Clk       (Clk, pin 5) connected to PA2
+// 3.3V          (Vcc, pin 6) power
+// back light    (BL,  pin 7) not connected, consists of 4 white LEDs which draw ~80mA total
+// Ground        (Gnd, pin 8) ground
+
+// Red SparkFun Nokia 5110 (LCD-10168)
+// -----------------------------------
+// Signal        (Nokia 5110) LaunchPad pin
+// 3.3V          (VCC, pin 1) power
+// Ground        (GND, pin 2) ground
+// SSI0Fss       (SCE, pin 3) connected to PA3
+// Reset         (RST, pin 4) connected to PA7
+// Data/Command  (D/C, pin 5) connected to PA6
+// SSI0Tx        (DN,  pin 6) connected to PA5
+// SSI0Clk       (SCLK, pin 7) connected to PA2
+// back light    (LED, pin 8) not connected, consists of 4 white LEDs which draw ~80mA total
+
+#include "tm4c123gh6pm.h"
+#include "Nokia5110.h"
+#include "Random.h"
+#include "TExaS.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Timer2_Init(unsigned long period);
+void Delay100ms(unsigned long count); // time delay in 0.1 seconds
+unsigned long TimerCount;
+unsigned long Semaphore;
+
+
+// *************************** Images ***************************
+// width=16 x height=10
+
+#define BOARD_ROWS 6
+#define BOARD_COLS 7
+
+void printBoard(char *board);
+int takeTurn(char *board, int player, const char*);
+int checkWin(char *board);
+int checkFour(char *board, int, int, int, int);
+int horizontalCheck(char *board);
+int verticalCheck(char *board);
+int diagonalCheck(char *board);
+
+/*
+void Draw(int dis){
+	Nokia5110_ClearBuffer();
+	Nokia5110_PrintBMP(0, ENEMY10H - 1 + dis, SmallEnemy10PointA, 0);
+  Nokia5110_PrintBMP(16, ENEMY10H - 1 + dis, SmallEnemy20PointA, 0);
+  Nokia5110_PrintBMP(32, ENEMY10H - 1 + dis, SmallEnemy20PointA, 0);
+  Nokia5110_PrintBMP(48, ENEMY10H - 1 + dis, SmallEnemy30PointA, 0);
+  Nokia5110_PrintBMP(64, ENEMY10H - 1 + dis, SmallEnemy30PointA, 0);
+  Nokia5110_DisplayBuffer();     // draw buffer
+}*/
+
+
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
+
+// basic functions defined at end of startup.s
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+unsigned long SW1,SW2;  // input from PF4,PF0
+unsigned long Out;      // outputs to PF3,PF2,PF1 (multicolor LED)
+void PortF_Init(void);
+void Delay(void);
+void s0 (void) ; 
+int s1 (void) ;
+int s2 (void) ;
+
+int y = 0, f = 0 , i = 0 , j = 0 ;
+int turn, done = 0;
+const char *PIECES = "XO";
+char board[BOARD_ROWS * BOARD_COLS];
+
+
+void PortF_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay   
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0        
+}
+
+
+
+int main(void){
+  TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
+  // initialization goes here
+
+	  PortF_Init();        // Call initialization of port PF4, PF3, PF2, PF1, PF0  
+		
+	for(i = 0 ; i < BOARD_COLS*BOARD_ROWS;i++)
+	{
+		if(i>3*BOARD_COLS)
+		board[i] = '|';
+		else
+			board[i]='*';
+	}
+		
+  EnableInterrupts();           // enable interrupts for the grader
+	Random_Init(1);
+  Nokia5110_Init();
+  Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();      // draw buffer
+	printBoard(board);
+
+  
+  
+
+}
+
+void printBoard(char *board){
+   int row, col;
+	Nokia5110_ClearBuffer();
+   for(row = 0; row < BOARD_ROWS; row++){
+      for(col = 0; col < BOARD_COLS; col++){
+				Nokia5110_SetCursorChar( col ,row,board[BOARD_COLS*row+col]);
+				Delay100ms(3);
+      }
+   }
+	 //Nokia5110_DisplayBuffer();
+
+}
+
+// You can use this timer only if you learn how it works
+void Timer2_Init(unsigned long period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  TimerCount = 0;
+  Semaphore = 0;
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
+void Timer2A_Handler(void){ 
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+  TimerCount++;
+  Semaphore = 1; // trigger
+}
+void Delay100ms(unsigned long count){unsigned long volatile time;
+  while(count){
+    time = 72724;  // 0.1sec at 80 MHz
+    while(time){
+	  	time--;
+    }
+    count--;
+  }
+}
+
+/*Nokia5110_PrintBMP(32, 47, PlayerShip0, 0); // player ship middle bottom
+  Nokia5110_PrintBMP(33, 47 - PLAYERH, Bunker0, 0);
+	while(1){
+		while(!(GPIO_PORTF_DATA_R&0x10)){
+			y = ENEMY10H + y == 47 ? 47 - ENEMY10H : y + 1;
+			Draw(y);
+			Delay100ms(1);
+		}
+		while(!(GPIO_PORTF_DATA_R&0x01)){
+			y = !y ? 0 : y - 1;
+			Draw(y);
+			Delay100ms(1);
+		}
+		
+	}*/
+//  Delay100ms(50);              // delay 5 sec at 50 MHz
+
+/*
+Nokia5110_Clear();
+  Nokia5110_SetCursor(1, 1);
+  Nokia5110_OutString("GAME OVER");
+  Nokia5110_SetCursor(1, 2);
+  Nokia5110_OutString("Nice try,");
+  Nokia5110_SetCursor(1, 3);
+  Nokia5110_OutString("Earthling!");
+  Nokia5110_SetCursor(2, 4);
+  Nokia5110_OutUDec(1234);
+  
+	*/
---
+++ connect4.d
@@ -0,0 +1,5 @@
+.\connect4.o: Connect4.c
+.\connect4.o: tm4c123gh6pm.h
+.\connect4.o: Nokia5110.h
+.\connect4.o: Random.h
+.\connect4.o: TExaS.h
